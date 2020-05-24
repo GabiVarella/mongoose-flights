5b505fb8ddf85c35fb6063962411a858e94c2044
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-05-24 11:42:33 -0700

    add basic bootstrap

--- controllers/flights.js
+++ controllers/flights.js
@@ -10,7 +10,6 @@ module.exports = {
 
 function addDest(req, res){
     Flight.findById(req.params.id, function(err, flight){
-        console.log(req.body);
         flight.destinations.push(req.body);
         flight.save(function(err){
             res.redirect(`/flights/${req.params.id}`);
